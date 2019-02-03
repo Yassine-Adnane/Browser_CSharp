1e6bd571d57fca1c2fa88bf598db7b8f20bf3bea
Yassine-Adnane <35614932+Yassine-Adnane@users.noreply.github.com>
2019-02-03 17:04:19 +0100

    Browser

---
+++ BrowserCsharp/BrowserCsharp/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace BrowserCsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1180, 768);
+            this.panel1.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1180, 768);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 792);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ BrowserCsharp/BrowserCsharp/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrowserCsharp
+{
+    public partial class Form1 : Form
+    {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams Prmtr = base.CreateParams;
+                Prmtr.ClassStyle |= 0x200;
+                return Prmtr;
+            }
+        }
+
+        private int _Ticks;
+        Timer timer = new Timer();
+
+        public Form1()
+        {
+            _Ticks = 0;
+
+            InitializeComponent();
+            webBrowser1.Navigate("https://www.youtube.com/?hl=FR");
+
+            
+            timer.Interval = (10 * 1000); // 10 secs
+            timer.Tick += new EventHandler(timer1_Tick);
+            timer.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*
+            _Ticks++;
+            if(_Ticks == 10)
+            {
+                webBrowser1.Navigate("https://mail.google.com/mail/u/0/?tab=wm#inbox");
+                _Ticks = 0;
+            }
+            */
+            webBrowser1.Navigate("https://mail.google.com/mail/u/0/?tab=wm#inbox");
+            //webBrowser1.Navigate("https://www.youtube.com/?hl=FR");
+
+        }
+    }
+}
